51d1d8e046ddbe7294fad8eec97004220dd3b474
Adam Mitro <adam.mitro@gmail.com>
2025-01-19 17:32:15 -0500

    Start adding effectiveness chart modal

--- app/src/main/java/com/example/pokedex_android/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokedex_android/pokemondetail/PokemonDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.pokedex_android.pokemondetail
 
+import PokemonEffectiveness
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
@@ -23,6 +24,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -47,12 +49,14 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import com.example.pokedex_android.R
 import com.example.pokedex_android.data.remote.responses.Pokemon
+import com.example.pokedex_android.ui.theme.RobotoCondensed
 import com.example.pokedex_android.util.Resource
 import com.plcoding.jetpackcomposepokedex.util.parseStatToAbbr
 import com.plcoding.jetpackcomposepokedex.util.parseStatToColor
@@ -124,7 +128,8 @@ fun PokemonDetailScreen(
                     start = 16.dp,
                     end = 16.dp,
                     bottom = 16.dp
-                )
+                ),
+            viewModel = viewModel
         )
         Box(contentAlignment = Alignment.TopCenter,
             modifier = Modifier
@@ -187,7 +192,8 @@ fun PokemonDetailStateWrapper(
     pokemonInfo: Resource<Pokemon>,
     navController: NavController,
     modifier: Modifier = Modifier,
-    loadingModifier: Modifier = Modifier
+    loadingModifier: Modifier = Modifier,
+    viewModel: PokemonDetailViewModel
 ) {
     when(pokemonInfo) {
         is Resource.Success -> {
@@ -195,7 +201,8 @@ fun PokemonDetailStateWrapper(
                 pokemonInfo = pokemonInfo.data!!,
                 navController = navController,
                 modifier = modifier
-                    .offset(y = (-20).dp)
+                    .offset(y = (-20).dp),
+                viewModel = viewModel
             )
         }
         is Resource.Error -> {
@@ -219,7 +226,7 @@ fun PokemonDetailSection(
     pokemonInfo: Pokemon,
     navController: NavController,
     modifier: Modifier = Modifier,
-    viewModel: PokemonDetailViewModel = hiltViewModel()
+    viewModel: PokemonDetailViewModel
 ) {
     val pokemonInfoLocal = viewModel.localPokemonData.value[pokemonInfo.id - 1]
 
@@ -259,6 +266,11 @@ fun PokemonDetailSection(
             )
         }
         PokemonTypeSection(types = viewModel.localPokemonData.value[pokemonInfo.id - 1].type)
+        PokemonEffectivenessSection(
+            currentPokemonTypes = viewModel.localPokemonData.value[pokemonInfo.id - 1].type,
+            typeEffectiveness = viewModel.pokemonEffectivenessData.value,
+            pokemonInfo = pokemonInfo,
+            viewModel = viewModel)
         PokemonDetailDataSection(
             pokemonWeight = pokemonInfo.weight,
             pokemonHeight = pokemonInfo.height
@@ -267,7 +279,7 @@ fun PokemonDetailSection(
             species = pokemonInfoLocal.species,
             description = pokemonInfoLocal.description
         )
-        PokemonEvolutionSection(navController = navController)
+        PokemonEvolutionSection(viewModel = viewModel, navController = navController)
         PokemonBaseStats(pokemonInfo = pokemonInfo)
     }
 }
@@ -376,7 +388,7 @@ fun PokemonDescriptionSection(
 
 @Composable
 fun PokemonEvolutionSection(
-    viewModel: PokemonDetailViewModel = hiltViewModel(),
+    viewModel: PokemonDetailViewModel,
     navController: NavController,
     modifier: Modifier = Modifier
 ) {
@@ -606,10 +618,99 @@ fun PokemonBaseStats(
     }
 }
 
-//@Composable
-//fun PokemonEffectiveness(
-//    types: List<String>,
-//    typeEffectiveness: Map<String, Float>
-//) {
-//
-//}
+@Composable
+fun PokemonEffectivenessSection(
+    currentPokemonTypes: List<String>,
+    typeEffectiveness: PokemonEffectiveness?,
+    pokemonInfo: Pokemon,
+    viewModel: PokemonDetailViewModel
+) {
+    var showModal = remember { mutableStateOf(false) }
+
+    Box {
+        Column(modifier = Modifier) {
+            Button(
+            onClick = { showModal.value = !showModal.value }
+            ) {
+                Text(text = "Show Effectiveness Chart")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (showModal.value) {
+                EffectivenessChart(onDismiss = { showModal.value = false }, pokemonInfo = pokemonInfo, types = currentPokemonTypes, viewModel = viewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun EffectivenessChart(
+    onDismiss: () -> Unit,
+    pokemonInfo: Pokemon,
+    types: List<String>,
+    viewModel: PokemonDetailViewModel
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .shadow(10.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.White)
+        ) {
+            Column(modifier = Modifier.padding(24.dp)) {
+                Row(horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()) {
+                    SubcomposeAsyncImage(
+                        model = pokemonInfo.sprites.front_default,
+                        contentDescription = pokemonInfo.name,
+                        success = { success ->
+                            viewModel.calcDominantColor(success.result.drawable) {
+                                viewModel.dominantColor.value = it
+                            }
+                            SubcomposeAsyncImageContent()
+                        },
+                        modifier = Modifier
+                            .size(100.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "${pokemonInfo.name.replaceFirstChar { it.uppercase() }} Type(s) ",
+                    fontFamily = RobotoCondensed,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth())
+
+                PokemonTypeSection(types = types)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Offense",
+                    fontFamily = RobotoCondensed,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "Defense",
+                    fontFamily = RobotoCondensed,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+            }
+        }
+    }
+}
